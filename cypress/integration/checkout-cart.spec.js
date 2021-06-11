f80979347d4969d4a1820451294dc4834b8a79b7
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-06-12 00:48:15 +0700

    fixed element not detached
    Thanks to https://stackoverflow.com/questions/64421563/cypress-wait-for-unpredictable-component-to-render-from-an-exclusive-set

--- cypress/integration/checkout-cart.spec.js
+++ cypress/integration/checkout-cart.spec.js
@@ -1,31 +1,43 @@
 /// <reference types="Cypress" />
-import 'cypress-wait-until';
 
 describe('Checkout Cart', () => {
-	it('visits Pomelo website', () => {
+	beforeEach(() => {
 		cy.visit('/');
 	});
 
-	it('clicks login button', () => {
+	it('Checks out cart', () => {
 		cy.get('[data-cy=auth__login__menu]').click();
-	});
-
-	it('enters e-mail and password', () => {
 		cy.get('input[name=email]').type('saint12302@gmail.com');
 		cy.get('input[name=password]').type('Pomelo123');
-	});
-
-	it('clicks submit login button', () => {
 		cy.get('[data-cy=auth__login__email__button]').click();
-	});
-
-	it('navigates to all clothing section', () => {
-		cy.waitUntil(() =>
-			cy.getCookie('token').then((cookie) => !!(cookie && cookie.value))
-		);
-		// cy.wait(10000);
 		cy.get('li[data-cy=nav_desktop__shop]', {
-			timeout: 10000,
-		}).click();
+			timeout: 100000,
+		}).should('be.visible').click();
 	});
+	// it('visits Pomelo website', () => {
+	// 	cy.visit('/');
+	// });
+
+	// it('clicks login button', () => {
+	// 	cy.get('[data-cy=auth__login__menu]').click();
+	// });
+
+	// it('enters e-mail and password', () => {
+	// 	cy.get('input[name=email]').type('saint12302@gmail.com');
+	// 	cy.get('input[name=password]').type('Pomelo123');
+	// });
+
+	// it('clicks submit login button', () => {
+	// 	cy.get('[data-cy=auth__login__email__button]').click();
+	// });
+
+	// it('navigates to all clothing section', () => {
+	// 	cy.waitUntil(() =>
+	// 		cy.getCookie('token').then((cookie) => !!(cookie && cookie.value))
+	// 	);
+	// 	// cy.wait(10000);
+	// 	cy.get('li[data-cy=nav_desktop__shop]', {
+	// 		timeout: 10000,
+	// 	}).click();
+	// });
 });
