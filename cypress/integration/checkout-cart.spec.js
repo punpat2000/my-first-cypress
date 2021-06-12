61befc716e9a503c45c2aeb53bc7c038087c401b
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-06-13 02:17:30 +0700

    add 'clear cookies and local storage'

--- cypress/integration/checkout-cart.spec.js
+++ cypress/integration/checkout-cart.spec.js
@@ -1,16 +1,27 @@
 /// <reference types="Cypress" />
 import { Auth, ShopNavigation, Shopping } from '../support/utils';
 
-describe('Checkout Cart', () => {
+describe('Checkout cart', () => {
 	beforeEach(() => {
 		cy.log('visit Pomelo Thailand landing page');
 		cy.visit('/');
 	});
 
-	it('Checks out cart', () => {
+	afterEach(() => {
+		cy.clearCookies();
+		cy.clearLocalStorage();
+	});
+
+	it('Checks out cart while signed in', () => {
 		Auth.toggleLogginInterface().logginByEmail();
 		ShopNavigation.toggleShopInterface().goTo('All Clothing');
-		cy.wait(10000);
+		cy.wait(15000);
 		Shopping.pickFirstItem().addToBag().checkOut();
 	});
+
+	// it('Checks out cart while unsigned', () => {
+	// 	ShopNavigation.toggleShopInterface().goTo('All Clothing');
+	// 	cy.wait(15000);
+	// 	Shopping.pickFirstItem().addToBag().checkOut();
+	// });
 });
